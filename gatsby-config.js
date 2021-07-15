c0020d43f69177e04323816a968ceed18d4fef42
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-07-15 17:59:03 +0200

    Fix RSS feed generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,8 +71,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-feed`,
       options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
         feeds: [
           {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.description,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
+              });
+            },
             query: `
               {
                 allMarkdownRemark(
