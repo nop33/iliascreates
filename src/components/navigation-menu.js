500beaeaa7e5ea1785dab3a29e3d321fbe3d2d99
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-07-29 15:25:27 +0200

    Add link to CV

--- src/components/navigation-menu.js
+++ src/components/navigation-menu.js
@@ -32,6 +32,9 @@ const NavigationMenu = ({ isHome }) => {
         <li className={navigationMenuItem}>
           <Link to="/blog/">Dev blog</Link>
         </li>
+        <li className={navigationMenuItem}>
+          <a href="https://cv.iliascreates.com/">CV</a>
+        </li>
       </ul>
     </div>
   );
