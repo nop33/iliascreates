542517769c6d4bbf5f2ad91d33e4ff719c7fdd8f
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-04-02 17:20:38 +0200

    Improve font sizes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,8 @@
 // custom typefaces
 import "@fontsource/andada-pro";
+// Highlighting for code blocks
+import "prismjs/themes/prism.css";
 // normalize CSS across browsers
 import "./src/normalize.css";
 // custom CSS styles
 import "./src/style.scss";
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css";
